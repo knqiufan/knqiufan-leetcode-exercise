e32996c6c4e09cc6af53a16d347ccbfb2a8aef86
knqiufan <knqiufan@foxmail.com>
2024-03-23 22:50:17 +0800

    add merge order array method

--- src/main/java/com/knqiufan/leetcode/MergedOrderedArray.java
+++ src/main/java/com/knqiufan/leetcode/MergedOrderedArray.java
@@ -1,8 +1,73 @@
-package com.knqiufan.leetcode;/**
- * 类描述
- * 
+package com.knqiufan.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ *
  * @author knqiufan
  * @version 1.0.0
  * @date 2024/3/23 1:35
-*/public class MergedOrderedArray {
+ */
+public class MergedOrderedArray {
+  public static void main(String[] args) {
+    int[] nums1 = new int[]{4, 5, 6, 0, 0, 0};
+    int[] nums2 = new int[]{1, 2, 3};
+    int m = 3;
+    int n = 3;
+    // System.out.println(Arrays.toString(merge(nums1, m, nums2, n)));
+    System.out.println(Arrays.toString(merge2(nums1, m, nums2, n)));
+  }
+
+  /**
+   * 直接合并后排序
+   * 执行用时分布 1 ms
+   * 击败 28.39% 使用 Java 的用户
+   */
+  public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+    System.arraycopy(nums2, 0, nums1, m, n);
+    int temp = 0;
+    for (int i = 0; i < nums1.length - 1; i++) {
+      for (int j = 0; j < nums1.length - i - 1; j++) {
+        if (nums1[j] > nums1[j + 1]) {
+          temp = nums1[j];
+          nums1[j] = nums1[j + 1];
+          nums1[j + 1] = temp;
+        }
+      }
+    }
+    return nums1;
+  }
+
+  /**
+   * 双指针
+   * 执行用时分布 0 ms
+   * 击败 100.00% 使用 Java 的用户
+   */
+  public static int[] merge2(int[] nums1, int m, int[] nums2, int n) {
+    // 设置 nums1 的头指针和 nums2 的头指针
+    int p1 = 0, p2 = 0;
+    int[] sorted = new int[m + n];
+    int cur;
+    while (p1 < m || p2 < n) {
+      if (p1 == m) {
+        cur = nums2[p2++];
+      } else if (p2 == n) {
+        cur = nums1[p1++];
+      } else if (nums1[p1] < nums2[p2]) {
+        cur = nums1[p1++];
+      } else {
+        cur = nums2[p2++];
+      }
+      sorted[p1 + p2 - 1] = cur;
+    }
+    // System.arraycopy 占用内存更高
+    // System.arraycopy(sorted, 0, nums1, 0, nums1.length);
+    for (int i = 0; i < sorted.length; i++) {
+      nums1[i] = sorted[i];
+    }
+    return nums1;
+  }
 }
--- src/main/java/com/knqiufan/leetcode/NumberSum.java
+++ src/main/java/com/knqiufan/leetcode/NumberSum.java
@@ -1,8 +1,113 @@
-package com.knqiufan.leetcode;/**
- * 类描述
- * 
+package com.knqiufan.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 题目：
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
  * @author knqiufan
  * @version 1.0.0
  * @date 2024/3/22 23:46
-*/public class NumberSum {
+ */
+public class NumberSum {
+  public static void main(String[] args) {
+    int[] nums = new int[]{2, 2, 1};
+    int target = 4;
+    System.out.println(Arrays.toString(twoSum(nums, target)));
+    System.out.println(Arrays.toString(twoSum2(nums, target)));
+    System.out.println(Arrays.toString(twoSum3(nums, target)));
+    System.out.println(Arrays.toString(twoSum4(nums, target)));
+  }
+
+  /**
+   * 执行用时 115 ms（击败 5.02% 使用 Java 的用户）
+   */
+  public static int[] twoSum(int[] nums, int target) {
+    int[] result = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      result[0] = i;
+      for (int j = 0; j < nums.length; j++) {
+        if (i == j) {
+          continue;
+        }
+        if (nums[i] + nums[j] == target) {
+          result[1] = j;
+          break;
+        }
+      }
+      if (result[1] != 0) {
+        break;
+      }
+    }
+    return result;
+  }
+
+  /**
+   * 暴力枚举 执行用时分布 55 ms
+   * 击败 16.10% 使用 Java 的用户
+   */
+  public static int[] twoSum2(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return new int[]{i, j};
+        }
+      }
+    }
+    return new int[]{0, 0};
+  }
+
+  /**
+   * 哈希表
+   * 执行用时分布 2 ms
+   * 击败 97.84% 使用 Java 的用户
+   * @param nums
+   * @param target
+   * @return
+   */
+  public static int[] twoSum3(int[] nums, int target) {
+    Map<Integer, Integer> hashtable = new HashMap<>();
+    for (int i = 0; i < nums.length; ++i) {
+      if (hashtable.containsKey(target - nums[i])) {
+        return new int[]{hashtable.get(target - nums[i]), i};
+      }
+      hashtable.put(nums[i], i);
+    }
+    return new int[0];
+  }
+
+  /**
+   * 哈希表，升级版
+   * 执行用时分布 0 ms
+   * 击败 100.00% 使用 Java 的用户
+   */
+  public static int[] twoSum4(int[] nums, int target) {
+    Map<Integer, Integer> hashtable = new HashMap<>();
+    for (int i = 0; i < nums.length; ++i) {
+      int j = nums.length - i - 1;
+      if (i == j) {
+        if (hashtable.containsKey(target - nums[i])) {
+          return new int[]{hashtable.get(target - nums[i]), i};
+        }
+      }
+      int numi = target - nums[i];
+      int numj = target - nums[j];
+      if (numj + numi == target) {
+        return new int[]{i, j};
+      }
+      if (hashtable.containsKey(numi)) {
+        return new int[]{hashtable.get(numi), i};
+      }
+      if (hashtable.containsKey(numj)) {
+        return new int[]{hashtable.get(numj), j};
+      }
+      hashtable.put(nums[i], i);
+      hashtable.put(nums[j], j);
+    }
+    return new int[0];
+  }
 }
