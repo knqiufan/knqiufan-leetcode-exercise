cad0065d83cd0d38eeff7ddf2909fce7343d66d4
knqiufan <knqiufan@foxmail.com>
2025-01-08 23:00:15 +0800

    update leetcode number 14

---
+++ src/main/java/com/knqiufan/leetcode/array/LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+package com.knqiufan.leetcode.array;
+
+/**
+ * 14. 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * @author knqiufan
+ * @version 1.0.0
+ * @date 2025/1/8 22:57
+ */
+public class LongestCommonPrefix {
+
+  public static void main(String[] args) {
+
+  }
+}
