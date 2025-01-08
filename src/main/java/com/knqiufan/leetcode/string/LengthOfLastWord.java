b030cfb88c370724ab502f8bb32076f55376a57d
knqiufan <knqiufan@foxmail.com>
2025-01-08 22:49:01 +0800

    add leetcode number 58

---
+++ src/main/java/com/knqiufan/leetcode/string/LengthOfLastWord.java
@@ -0,0 +1,15 @@
+package com.knqiufan.leetcode.string;
+
+/**
+ * 58. 最后一个单词的长度
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+ * <p>
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串
+ * 。
+ *
+ * @author knqiufan
+ * @version 1.0.0
+ * @date 2025/1/8 22:47
+ */
+public class LengthOfLastWord {
+}
