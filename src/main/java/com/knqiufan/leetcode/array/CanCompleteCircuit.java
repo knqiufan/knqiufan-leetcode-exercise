081c7f564d8147b1a589722645cd17614a18978f
knqiufan <knqiufan@foxmail.com>
2025-01-11 11:44:12 +0800

    update leetcode 134

--- src/main/java/com/knqiufan/leetcode/array/CanCompleteCircuit.java
+++ src/main/java/com/knqiufan/leetcode/array/CanCompleteCircuit.java
@@ -1,5 +1,7 @@
 package com.knqiufan.leetcode.array;
 
+import java.util.Scanner;
+
 /**
  * 134. 加油站
  * 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
@@ -14,6 +16,55 @@ package com.knqiufan.leetcode.array;
  */
 public class CanCompleteCircuit {
   public static void main(String[] args) {
+    int[] gas = new int[]{5,1,2,3,4};
+    int[] cost = new int[]{4,4,1,5,1};
+    System.out.println(canCompleteCircuit(gas, cost));
+  }
 
+  public static int canCompleteCircuit(int[] gas, int[] cost) {
+    int begin = -1;
+    // 计算 gas 和 cost 的 sum
+    int sumGas = sum(gas);
+    int sumCost = sum(cost);
+    if (sumCost > sumGas) {
+      return -1;
+    }
+    // 递归
+    return b(gas, cost, begin, 0);
+  }
+
+  private static int b(int[] gas, int[] cost, int begin, int i) {
+    if (i == gas.length) {
+      return begin;
+    }
+    int next;
+    int sheng = 0;
+    sheng += gas[i];
+    next = a(sheng, cost);
+    if (next == -1) {
+      b(gas, cost, begin, i + 1);
+    } else if (begin == -1) {
+      begin = next;
+    } else {
+      b(gas, cost, begin, next);
+    }
+    return begin;
+  }
+
+  private static int a(int sheng, int[] cost) {
+    for (int i = 0; i < cost.length; i++) {
+      if (sheng >= cost[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  private static int sum(int[] arr) {
+    int sum = 0;
+    for (int i : arr) {
+      sum += i;
+    }
+    return sum;
   }
 }
