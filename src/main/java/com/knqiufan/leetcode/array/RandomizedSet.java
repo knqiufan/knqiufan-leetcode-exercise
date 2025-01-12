8e932438ec60672f8d5b14ff851e847ee89061cf
knqiufan <knqiufan@foxmail.com>
2025-01-12 22:11:29 +0800

    add leetcode 380

---
+++ src/main/java/com/knqiufan/leetcode/array/RandomizedSet.java
@@ -0,0 +1,18 @@
+package com.knqiufan.leetcode.array;
+
+/**
+ * 380. O(1) 时间插入、删除和获取随机元素
+ * 实现RandomizedSet 类：
+ * <p>
+ * RandomizedSet() 初始化 RandomizedSet 对象
+ * bool insert(int val) 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false 。
+ * bool remove(int val) 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false 。
+ * int getRandom() 随机返回现有集合中的一项（测试用例保证调用此方法时集合中至少存在一个元素）。每个元素应该有 相同的概率 被返回。
+ * 你必须实现类的所有函数，并满足每个函数的 平均 时间复杂度为 O(1) 。
+ *
+ * @author knqiufan
+ * @version 1.0.0
+ * @date 2025/1/12 22:10
+ */
+public class RandomizedSet {
+}
